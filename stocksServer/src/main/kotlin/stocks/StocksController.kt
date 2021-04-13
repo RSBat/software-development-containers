c65e91b60dd10b557e44cc85413da60b03ed02c7
Sergei Kozelko <kozelkos@yandex.ru>
2021-04-13 20:39:39 +0300

    Implement exchange server

--- stocksServer/src/main/kotlin/stocks/StocksController.kt
+++ stocksServer/src/main/kotlin/stocks/StocksController.kt
@@ -1,29 +1,62 @@
 package stocks
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class StocksController {
-    private val stockPrices: Map<Long, Long> = mutableMapOf()
+    private val stockPrices = mutableMapOf<String, Long>()
+    private val stockAmount = mutableMapOf<String, Long>()
 
     @GetMapping("/info/stocks")
-    fun getStocksInfo(): String {
-        return ""
+    fun getStocksInfo(): List<StocksItem> {
+        return stockPrices.map { StocksItem(it.key, it.value, stockAmount[it.key]!!) }
     }
 
     @GetMapping("/buy")
-    fun buyStocks(): String {
-        return ""
+    fun buyShares(
+        @RequestParam("name")
+        name: String,
+        @RequestParam("amount")
+        amount: Long,
+        @RequestParam("maxPrice")
+        maxPrice: Long
+    ): String {
+        if (stockAmount.getOrDefault(name, 0) < amount) {
+            return "Not enough shares available"
+        }
+        val totalPrice = stockPrices.getOrDefault(name, 0) * amount
+        if (totalPrice > maxPrice) {
+            return "Max price exceeded"
+        }
+        stockAmount.compute(name) { _, oldAmount -> oldAmount!! - amount }
+        return totalPrice.toString()
     }
 
     @GetMapping("/sell")
-    fun sellStock(): String {
-        return ""
+    fun sellShares(
+        @RequestParam("name")
+        name: String,
+        @RequestParam("amount")
+        amount: Long
+    ): String {
+        val totalPrice = stockPrices.getOrDefault(name, 0) * amount
+        stockAmount.compute(name) { _, oldAmount -> (oldAmount ?: 0) + amount }
+        return totalPrice.toString()
     }
 
     @GetMapping("/set")
-    fun setPrice(): String {
-        return ""
+    fun setStock(
+        @RequestParam("name")
+        name: String,
+        @RequestParam("price")
+        price: Long,
+        @RequestParam("amount")
+        amount: Long
+    ): String {
+        stockPrices[name] = price
+        stockAmount[name] = amount
+        return "ok"
     }
 }
